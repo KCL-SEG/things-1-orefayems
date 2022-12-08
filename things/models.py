ac4e06541691277cf5aa00dcf1b635d487e437cb
Oreoluwa Fayemiwo <oreoluwa.fayemiwo@kcl.ac.uk>
2022-12-08 19:46:06 +0000

    Complete Things

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.vlidators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Thing(models.Model):
+	REQUIRED_FIELDS = ('thing',)
+	
+	name = models.CharField(max_length=30, unique=True, blank=False)
+	description = model.CharField(max_length=120, unique=False, blank=True)
+	quantity = models.SmallIntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
--- things/views.py
+++ things/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-<html>
-<head>
-<title>Things</title>
-</head>
-
-<h1>Things</h1>
-
-
-
-
-</html>
+def home(request):
+	return render(request, 'home.html')
